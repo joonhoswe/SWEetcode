116dfc8bdbe33641cf048dcb2be9c92215faf17a
joonhoswe <joonhoswe@gmail.com>
2024-03-13 14:41:46 -0400

    setup tailwind styling

--- frontend/index.js
+++ frontend/index.js
@@ -1,23 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style = {styles.header}> SWEetcode </Text>
-      <StatusBar style="auto" />
-    </View>
+
+      <View style = {tw`bg-gray-800 flex-1 items-center justify-center`}>
+        <Text style = {tw`text-white text-3xl`}>
+            SWEetcode
+        </Text>
+      </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#3A3B3C',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    color: "white",
-  }
-});
